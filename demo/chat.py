a75f1d3f8cd84a2e87d0daf7569c278ccc6a2534
CoderWanFeng <875143930@qq.com>
2023-07-28 02:27:23 +0800

    add 免费搭建一个有脾气的聊天机器人，1行Python代码就够了！👉https://mp.weixin.qq.com/s/vJXMChkO2tCUEXpq8LlNCg

---
+++ demo/chat.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+'''
+@作者 ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫
+@微信 ：CoderWanFeng : https://mp.weixin.qq.com/s/yFcocJbfS9Hs375NhE8Gbw
+@个人网站 ：www.python-office.com
+@Date    ：2023/6/29 22:27 
+@Description     ：
+'''
+import porobot
+
+
+print(porobot.normal.chat("你好，我是程序员晚枫"))
---
+++ porobot/__init__.py
@@ -0,0 +1,1 @@
+from porobot.api import normal
